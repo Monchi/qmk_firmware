6758d5bec9cf8ba215eab562f2670f4553b4ddb9
Yuku Kotani <yukukotani@gmail.com>
2020-06-09 22:41:39 +0900

    Update keymap

--- keyboards/lily58/keymaps/yuku/rules.mk
+++ keyboards/lily58/keymaps/yuku/rules.mk
@@ -16,6 +16,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight. 
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_DRIVER_ENABLE= yes     # OLED display
+TAP_DANCE_ENABLE = yes      # Enable tap dance
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
